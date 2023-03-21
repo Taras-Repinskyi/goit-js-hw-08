88edd6ad9969e82434c0b75eeaa2683b7928db9e
Taras-Repinskyi <repinskiy.taras@gmail.com>
2023-03-21 20:14:24 +0200

    finish task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form'); 
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const savedFormState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+if (savedFormState) {
+  emailInput.value = savedFormState.email || '';
+  messageInput.value = savedFormState.message || '';
+}
+
+form.addEventListener('input', throttle(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({
+    email: emailInput.value,
+    message: messageInput.value
+  }));
+}, 500));
+
+form.addEventListener('submit', event => {
+  event.preventDefault(); 
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value 
+  });
+
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  form.reset();
+});
