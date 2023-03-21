1d0bc504641452e098edd065c7e6f66af09b2eb8
Taras-Repinskyi <repinskiy.taras@gmail.com>
2023-03-21 19:00:28 +0200

    fish task 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoPlayer = new Player('vimeo-player');
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+// Відновити відтворення відео зі збереженої позиції
+const savedTime = parseFloat(localStorage.getItem(LOCAL_STORAGE_KEY));
+if (!isNaN(savedTime)) {
+  videoPlayer.setCurrentTime(savedTime);
+}
+
+// Відстежувати оновлення часу відтворення і зберігати його у локальне сховище
+videoPlayer.on('timeupdate', throttle(event => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, event.seconds);
+}, 1000));
